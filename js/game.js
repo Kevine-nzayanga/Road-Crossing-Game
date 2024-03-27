9190a6eb8b9266a74d750568dd6a4a65c16b436d
Nzayanga-Keza <kevinenzayanga@gmail.com>
2024-03-27 11:49:10 +0300

    road crossing game

---
+++ js/game.js
@@ -0,0 +1,185 @@
+// create a new scene
+let gameScene = new Phaser.Scene('Game');
+
+gameScene.init = function(){
+  this.playerSPeed =3;
+
+  // enemy speed
+  this.enemyMinSpeed=2;
+  this.enemyMaxSpeed=3;
+
+  
+  this.enemyMinY = 80;
+  this.enemyMaxY= 280;
+
+  this.isTerminating = false;
+}
+
+gameScene.preload = function() {
+  this.load.image('background', 'assets/background.png');
+  this.load.image('player', 'assets/player.png');
+  this.load.image('enemy', 'assets/dragon.png');
+  this.load.image('goal', 'assets/treasure.png');
+
+}
+
+
+// called once after the preload ends
+gameScene.create = function() {
+  // create bg sprite
+  let bg = this.add.sprite(0, 0, 'background');
+
+  // change the origin to the top-left corner
+  bg.setOrigin(0,0);
+
+  this.player = this.add.sprite (40,this.sys.game.config.height/2, 'player');
+  this.player.setScale(0.5);
+
+
+  this.goal = this.add.sprite (this.sys.game.config.width-80, this.sys.game.config.height/2, 'goal').setScale(0.6);
+
+  // create enemy group
+  this.enemies = this.add.group(
+    {key:'enemy',
+    repeat:4,
+    setXY:{
+      x:90,
+      y:100,
+      stepX:100,
+      stepY:20
+    }
+    });
+    
+// this.enemy = this.add.sprite(120, this.sys.game.config.height / 2, 'enemy');
+// this.enemy.flipX = true;
+
+// this.enemies.add(this.enemy);
+
+console.log(this.enemies.getChildren());
+
+// change the scale of the group
+Phaser.Actions.ScaleXY (this.enemies.getChildren(),-0.4, -0.4);
+
+// set flipX enemy speed 
+Phaser.Actions.Call(this.enemies.getChildren(), function(enemy){
+enemy.flipX = true;
+
+// set enemy speed 
+let dir = Math.random() < 0.5 ? 1 : -1;
+let speed = this.enemyMinSpeed + Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed);
+enemy.speed = dir * speed;
+
+},this);
+
+
+
+
+  // this.enemy1 = this.add.sprite(250,180, 'enemy');
+  // this.enemy1.setScale(3);
+
+  // the logic setrotation can be used
+  // this.enemy1.rotation = Math.PI/4;
+
+// the logic of "angle ="" can be used
+  // enemy1.setAngle(45);
+  // enemy1.scaleX = 2;
+  // enemy1.scaleY = 2;
+
+  // let enemy2 = this.add.sprite(450,180, 'enemy');
+  // enemy2.displayWidth = 300;
+
+  // enemy.flipX= true;
+
+
+  // depth of 1 brings on top the sprite
+
+  // place sprite in the center
+  // bg.setPosition(640/2, 360/2);
+
+  // let gameW = this.sys.game.config.width;
+  // let gameH = this.sys.game.config.height;
+
+  // console.log(gameW, gameH);
+
+  // console.log(bg);
+  // console.log(this);
+  // console.log(this.enemy1);
+  
+};
+
+
+
+
+
+
+gameScene.update = function() {
+
+  if(this.input.activePointer.isDown){
+    this.player.x +=this.playerSPeed
+  }
+
+  let playerRect= this.player.getBounds();
+  let treasureRect = this.goal.getBounds();
+
+    // making sure the player doesnt go beyond the borders
+
+  if(Phaser.Geom.Intersects.RectangleToRectangle(playerRect, treasureRect)){
+    console.log('reached goal!');
+    return this.gameOver();
+  }
+
+
+
+
+// get enemies
+  let enemies = this.enemies.getChildren();
+  let numEnmenies = enemies.length;
+
+  for (let i = 0; i < numEnmenies; i++) {
+    enemies[i].y +=enemies[i].speed
+
+  // check we havent passed the minimum and maximum y
+  let conditionUp= enemies[i].speed <0 && enemies[i].y <= this.enemyMinY;
+  let conditionDown = enemies[i].speed >0 && enemies[i].y >= this.enemyMaxY;
+
+
+  if(conditionUp || conditionDown) {
+    enemies[i].speed *=-1;
+  }
+
+
+
+let enemyRect = enemies[i].getBounds()
+
+if(Phaser.Geom.Intersects.RectangleToRectangle(playerRect, enemyRect)){
+  console.log('game over!');
+  return this.gameOver();
+}
+
+  };
+
+
+  };
+
+  gameScene.gameOver = function(){
+    this.isTerminating= true;
+    this.cameras.main.shake(500);
+    this.cameras.main.on('camerashakecomplete', function (camera, effect){
+    this.scene.restart();
+
+    },this);
+
+  }
+
+
+
+// set the configuration of the game
+let config = {
+  type: Phaser.AUTO, // Phaser will use WebGL if available, if not it will use Canvas
+  width: 640,
+  height: 360,
+  scene: gameScene
+};
+
+// create a new game, pass the configuration
+let game = new Phaser.Game(config);
